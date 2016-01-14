a3b105895e41bfc1cf9fe41cd034de8c96bf9d28
Bo Jung <bo.jung@infusionsoft.com>
2016-01-13 20:44:43 -0700

    derp

---
+++ fizzbuzz.go
@@ -0,0 +1,22 @@
+
+
+package main
+
+import "fmt"
+// Herp is mod 3 0, Derp is mod 5 0
+
+func main() {
+	for i := 0; i <= 100; i++ {
+		switch {
+				case i%3 == 0 && i%5 == 0:
+					fmt.Println("FizzBuzz")
+				case i%3 == 0 && i%5 != 0:
+					fmt.Println("Fizz")
+				case i%3 != 0 && i%5 == 0:
+					fmt.Println("Buzz")
+				default:
+					fmt.Println(i)
+		}
+	
+	}
+}
